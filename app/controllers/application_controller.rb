d6406b00bbaa5cbfceedfd3e2e4536e900e93993
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-24 09:46:20 +0200

    login signup views & rails authorization with pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+
+  # Uncomment when you *get* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /^(active_)?admin/
+  end
+
+
 end
