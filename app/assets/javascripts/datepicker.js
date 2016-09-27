33ac48e5f30161bed79c0f611c6a873d4de4b74d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-27 08:50:14 +0200

    datepicker

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,13 @@
+function callDatePicker() {
+  $('#datepicker').datepicker({
+    autoclose: true,
+    format: "dd/mm/yyyy",
+    todayHighlight: true,
+    language: "fr",
+    daysOfWeekDisabled: "5,6",
+    orientation: "auto",
+    todayBtn: true,
+    startDate: 'today'
+  });
+}
+
---
+++ app/assets/javascripts/datetimepicker.js
@@ -0,0 +1,5 @@
+  $('.datetimepicker').datetimepicker({
+    inline: true,
+    format: "DD/MM/YYYY h:mm A",
+    stepping: 15
+  });
