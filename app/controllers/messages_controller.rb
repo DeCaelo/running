efddc33567b0d4b545a062b8c35a55047d0302a6
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-09-28 16:40:04 +0200

    message in event

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+  def create
+    @message = Message.new(content: params[:message][:content])
+    @event = Event.find(params[:event_id])
+    @user = current_user
+    @message.event = @event
+    @message.user = @user
+    authorize @message
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to event_path(@event) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'events/show' }
+        format.js  # <-- idem
+      end
+    end
+  end
 end
